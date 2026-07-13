6a40553e3f430d7dd425f09b0bc4a9edeeca3d46
Ms62870 <ms62870@gmail.com>
2026-07-14 01:15:21 +0530

    update

--- app/main.py
+++ app/main.py
@@ -33,7 +33,7 @@ async def start(message: Message):
 async def chat(message: Message):
     try:
         response = client.models.generate_content(
-            model="gemini-2.5-flash",
+            model="gemini-2.5-flash-lite",
             contents=message.text
         )
 
