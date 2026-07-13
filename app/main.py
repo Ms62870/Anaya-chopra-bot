c2d79b645fd0271b682f9f2a10e0a5a7b65ae414
Ms62870 <ms62870@gmail.com>
2026-07-14 00:39:39 +0530

    update

--- app/main.py
+++ app/main.py
@@ -1,17 +1,13 @@
 import os
 import asyncio
-
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN environment variable is missing")
-
-    bot = Bot(token=BOT_TOKEN)
-    
-BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
@@ -26,7 +22,9 @@ async def start(message: Message):
 
 @dp.message()
 async def echo(message: Message):
-    await message.answer(f"You said:\n\n{message.text}")
+    await message.answer(
+        f"You said:\n\n{message.text}"
+    )
 
 
 async def main():
@@ -36,4 +34,3 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
-                                
